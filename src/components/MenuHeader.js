aba76e5f607359c5df6af8d15727a7480b95c004
Joshua Clark <Joshrclark02@hotmail.com>
2024-02-05 17:55:20 -0600

    Fixed all the image issues?

--- src/components/MenuHeader.js
+++ src/components/MenuHeader.js
@@ -4,7 +4,7 @@ const MenuHeader = ({}) => {
     return ( 
       <div>
         <div class="row logo-row justify-content-center">
-          <img id="logo" src="/images/logo.png" alt="Logo for Human Food Place. The letters look like wheat."/>
+          <img id="logo" src={require("../images/logo.png")} alt="Logo for Human Food Place. The letters look like wheat."/>
         </div>
         <div class="row">
           <div class="col">
